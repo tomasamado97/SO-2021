6cb8f6aa8c30e8f967b883c41e52f6a397f9afc6
Tomas Amado <amado.tomas97@gmail.com>
2021-04-02 21:02:15 -0300

    first commit

---
+++ src/main/java/com/obligatorio1/obligatorio1/main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.obligatorio1.obligatorio1;
+
+/**
+ *
+ * @author tomasamado
+ */
+public class main {
+    
+}
