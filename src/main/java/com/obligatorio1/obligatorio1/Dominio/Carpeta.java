5f49ad346a6dbbd128d7be84ff34e69788ec1735
Tomas Amado <amado.tomas97@gmail.com>
2021-04-17 17:47:01 -0300

    agregue unas funciones y estructuras

--- src/main/java/com/obligatorio1/obligatorio1/Dominio/Carpeta.java
+++ src/main/java/com/obligatorio1/obligatorio1/Dominio/Carpeta.java
@@ -6,6 +6,9 @@
 package com.obligatorio1.obligatorio1.Dominio;
 
 public class Carpeta {
+    public String nombreDirecto;
     public Permiso permiso;
     public Carpeta[] carpetas;
+    public Carpeta carpetaPadre;
+    public Archivo[] archivos;
 }
--- src/main/java/com/obligatorio1/obligatorio1/Dominio/ControladorUsuario.java
+++ src/main/java/com/obligatorio1/obligatorio1/Dominio/ControladorUsuario.java
@@ -5,22 +5,28 @@
  */
 package com.obligatorio1.obligatorio1.Dominio;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-/**
- *
- * @author tomasamado
- */
 public class ControladorUsuario {
-    public Usuario[] usuarios;
+    public ArrayList<Usuario> usuarios;
+    public Usuario usuarioActual;
 
 
     public String userdel(String nombreUser){
-        if (usuarios.length == 0){
-            return "El usuario a borrar no existe";
-        }else{
-            usuarios = Arrays.stream(usuarios).filter(user -> user.nombreUsuario != nombreUser).toArray(Usuario[]::new);
+        boolean seBorroUsuario = usuarios.removeIf(user -> (user.nombreUsuario == nombreUser));
+        if (seBorroUsuario) {
             return "El usuario se borro con exito";
+        }else{
+            return "El usuario a borrar no existe";
+        }
+    }
+    
+    public String useradd(String nombreUser){
+        if (nombreUser.isEmpty()){
+            return "No se puede crear un usuario sin nombre";
+        }else{
+            Usuario nuevoUser = new Usuario();
+            usuarios.add(nuevoUser);
+            return "El usuario se agrego con exito";
         }
     }
 }
